a9b3763d2ceb5f0202e01eb84f97691da3a55a1f
Hong Le <hongrosebara@gmail.com>
2022-12-21 15:13:42 +0000

    row height

--- pages/index.js
+++ pages/index.js
@@ -3,54 +3,66 @@ import { useState } from "react";
 import { List } from "react-virtualized";
 import Autocomplete from "react-autocomplete";
 
-const rowRenderer = (data, onSelect) => ({ key, index, style }) => {
-  const client = data[index];
-  return (
-    <div key={key} style={style} onMouseDown={onSelect.bind(null, client)}>
-      <h2>{client.name}</h2>
-    </div>
-  );
-};
+const rowRenderer =
+  (data, onSelect) =>
+  ({ key, index, style }) => {
+    const client = data[index];
+    return (
+      <div key={key} style={style} onMouseDown={onSelect.bind(null, client)}>
+        <h2>{client.name}</h2>
+      </div>
+    );
+  };
 
 const renderMenu = (data, onSelect) => () => {
   return (
     <List
       width={500}
-      height={600}
-      rowHeight={50}
+      height={300}
+      rowHeight={20}
       rowCount={data.length}
       rowRenderer={rowRenderer(data, onSelect)}
     />
   );
 };
 
-
 const HomePage = ({ cities }) => {
   // default valse should be empty string
   const [searchTerm, setSearchTerm] = useState("");
 
   if (cities !== undefined) {
-    let data = cities; // show all items by default
+    let data = []; // show all items by default
 
-    if (searchTerm.length > 0) {
-      data = cities.filter(item =>
+    if (searchTerm.length > 1) {
+      data = cities.filter((city) =>
         // name.toLowerCase is used for case insensitivity
-        item.name.toLowerCase().includes(searchTerm.toLowerCase())
+        city.name.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
 
-    const onSelect = item => setSearchTerm(item.name);
+    const onSelect = (city) => setSearchTerm(city.name);
 
     return (
       <div className="flex items-center justify-center p-3">
         <Autocomplete
+          className="p-3"
           items={data}
           value={searchTerm}
-          getItemValue={item => item.name}
+          getItemValue={(item) => item.name}
           onChange={(e, value) => setSearchTerm(value)}
           renderMenu={renderMenu(data, onSelect)}
-          renderItem={item => <div>{item.name}</div>}
+          renderItem={(item) => <div>{item.name}</div>}
           shouldItemRender={() => false}
+          inputProps={{
+            className: "form-control",
+            style: {
+              width: "500px",
+              height: "25px",
+              padding: "2px",
+              background: "var(--color-bg-secondary)",
+            },
+            placeholder: "Search city",
+          }}
         />
       </div>
     );
